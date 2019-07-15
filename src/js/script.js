f4cf2c0f165cac43e03ba6d3a92ad0bf6af1cba2
Jose Guerra <jose@gcostudios.com>
2019-07-15 23:43:45 +0100

    updated header opacity script

--- src/js/script.js
+++ src/js/script.js
@@ -14,9 +14,14 @@ jQuery(function($) {
       this.headerOpacity();
     },
 
+    // Header Opacity Transition
+    // ==============================================
     headerOpacity: function() {
+      var $heroHeight = $('.hero-treatment').height(),
+          $heroSeventy = $heroHeight * 0.7;
+
       $(window).on('scroll', function() {
-        if ($(document).scrollTop() > 150 && $(this).width() >= 600) {
+        if ($(document).scrollTop() > $heroSeventy && $(this).width() >= 600) {
           $('#master-header').addClass('darken');
         } else {
           $('#master-header').removeClass('darken');
