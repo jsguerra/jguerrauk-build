1992da265709d0f02770bd61725435437adf211d
Jose Guerra <jose@gcostudios.com>
2020-12-08 11:31:03 +0000

    initial commit

---
+++ src/js/clock.js
@@ -0,0 +1,22 @@
+export const timer = () => {
+  let date = new Date(),
+      milliseconds = date.getMilliseconds(),
+      seconds = date.getSeconds(),
+      hours = date.getHours(),
+      minutes = date.getMinutes();
+
+  seconds += milliseconds / 1000; // using milliseconds smooths hand motions
+  minutes += seconds / 60;
+  hours += minutes / 60;
+
+  // Normalize to the 12 hour clock
+  if (hours > 12) {
+    hours -= 12;
+  }
+
+  clockElement.style.setProperty("--seconds", offset + 6 * seconds + "deg");
+  clockElement.style.setProperty("--hours", offset + 30 * hours + "deg");
+  clockElement.style.setProperty("--minutes", offset + 6 * minutes + "deg");
+
+  requestAnimationFrame(timer);
+}
