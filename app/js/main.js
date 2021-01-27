e51395baf0a9bd3d649884e1cab03a1d17c899f4
jsguerra <jose@gcostudios.com>
2021-01-27 20:24:11 +0000

    file updated

--- app/js/main.js
+++ app/js/main.js
@@ -1,28 +1,31 @@
 "use strict";
 
-(function () {
-  var clockElement = document.getElementById("analogClock");
-  var offset = -90;
+// Clock variables
+var clockElement = document.getElementById("analogClock");
+var offset = -90; // Clock Hand Rotation
 
-  var timer = function timer() {
-    var date = new Date(),
-        milliseconds = date.getMilliseconds(),
-        seconds = date.getSeconds(),
-        hours = date.getHours(),
-        minutes = date.getMinutes();
-    seconds += milliseconds / 1000; // using milliseconds smooths hand motions
+var clockRotate = function clockRotate(el, time) {
+  el.style.transform = "rotateZ(".concat(time, "deg)");
+}; // Clock function
 
-    minutes += seconds / 60;
-    hours += minutes / 60; // Normalize to the 12 hour clock
 
-    if (hours > 12) {
-      hours -= 12;
-    }
+var clock = function clock() {
+  var date = new Date(),
+      milliseconds = date.getMilliseconds(),
+      seconds = date.getSeconds(),
+      hours = date.getHours(),
+      minutes = date.getMinutes();
+  seconds += milliseconds / 1000; // using milliseconds smooths hand motions
 
-    clockElement.style.setProperty("--seconds", offset + 6 * seconds + "deg");
-    clockElement.style.setProperty("--hours", offset + 30 * hours + "deg");
-    clockElement.style.setProperty("--minutes", offset + 6 * minutes + "deg");
-    requestAnimationFrame(timer);
-  }; // timer();
+  minutes += seconds / 60;
+  hours += minutes / 60; // Normalize to the 12 hour clock
 
-})();
+  if (hours > 12) {
+    hours -= 12;
+  }
+
+  clockElement.style.setProperty("--seconds", offset + 6 * seconds + "deg");
+  clockElement.style.setProperty("--hours", offset + 30 * hours + "deg");
+  clockElement.style.setProperty("--minutes", offset + 6 * minutes + "deg");
+  requestAnimationFrame(clock);
+}; // clock();
