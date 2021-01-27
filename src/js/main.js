3bf4b2efb1e3f41491ee17324d0a21d47f507d4b
jsguerra <jose@gcostudios.com>
2021-01-27 20:24:04 +0000

    updated script

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,34 @@
-(function () {
-  const clockElement = document.getElementById("analogClock");
-  const offset = -90;
+// Clock variables
+const clockElement = document.getElementById("analogClock");
+const offset = -90;
 
-  const timer = () => {
-    let date = new Date(),
-        milliseconds = date.getMilliseconds(),
-        seconds = date.getSeconds(),
-        hours = date.getHours(),
-        minutes = date.getMinutes();
+// Clock Hand Rotation
+const clockRotate = (el, time) => {
+  el.style.transform = `rotateZ(${(time)}deg)`;
+}
 
-    seconds += milliseconds / 1000; // using milliseconds smooths hand motions
-    minutes += seconds / 60;
-    hours += minutes / 60;
+// Clock function
+const clock = () => {
+  let date = new Date(),
+      milliseconds = date.getMilliseconds(),
+      seconds = date.getSeconds(),
+      hours = date.getHours(),
+      minutes = date.getMinutes();
 
-    // Normalize to the 12 hour clock
-    if (hours > 12) {
-      hours -= 12;
-    }
+  seconds += milliseconds / 1000; // using milliseconds smooths hand motions
+  minutes += seconds / 60;
+  hours += minutes / 60;
 
-    clockElement.style.setProperty("--seconds", offset + 6 * seconds + "deg");
-    clockElement.style.setProperty("--hours", offset + 30 * hours + "deg");
-    clockElement.style.setProperty("--minutes", offset + 6 * minutes + "deg");
-
-    requestAnimationFrame(timer);
+  // Normalize to the 12 hour clock
+  if (hours > 12) {
+    hours -= 12;
   }
 
-  // timer();
-})();
+  clockElement.style.setProperty("--seconds", offset + 6 * seconds + "deg");
+  clockElement.style.setProperty("--hours", offset + 30 * hours + "deg");
+  clockElement.style.setProperty("--minutes", offset + 6 * minutes + "deg");
+
+  requestAnimationFrame(clock);
+}
+
+// clock();
